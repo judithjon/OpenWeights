20c26a05173275e7edd0b49f037e205c67aa5b1e
Geziena Hoogwerf <d872093f30@naoko.web.id>
2026-07-10 11:40:04 +0700

    Add main.go
    Co-authored-by: nancodero <nancodero@proton.me>

---
+++ cmd/openweights/main.go
@@ -0,0 +1,23 @@
+// cmd/openweights/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "openweights/internal/openweights"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := openweights.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
